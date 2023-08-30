04f4c4df0a1738e2503519a26068a785c52dbf29
M1cr0wave <73077505+M1cr0wave@users.noreply.github.com>
2023-08-30 12:57:56 +0300

    Create Question1.cpp

---
+++ C++/Beginner Questions/Question1.cpp
@@ -0,0 +1,15 @@
+/* Problem statement
+Your task is to display the following pattern on the screen:
+
+& & & & & &
+& & & & &
+& & & &
+& & &
+& &
+&
+
+
+
+Check the solution1.cpp for the answer to this
+*/
+
