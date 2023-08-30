b24fb3e6c88d3cfbb9f0b4c0994e27106add93b4
M1cr0wave <73077505+M1cr0wave@users.noreply.github.com>
2023-08-30 13:01:37 +0300

    Create Question2.cpp

---
+++ C++/Beginner Questions/Question2.cpp
@@ -0,0 +1,16 @@
+/*
+Problem statement
+In this challenge, your task is to:
+
+Declare a variable: int var
+Initialize var to 5000
+Print the value of the var
+Overwrite the value of var to 1000
+Print the value of the var again in the new line
+Output
+Your code should have the following output.
+
+var = 5000
+var = 1000
+
+*/
