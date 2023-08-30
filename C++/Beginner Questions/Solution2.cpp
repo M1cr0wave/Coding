c9f5b786b983dc10e7be0635c7d544716b63a9d1
M1cr0wave <73077505+M1cr0wave@users.noreply.github.com>
2023-08-30 13:04:19 +0300

    Create Solution2.cpp

---
+++ C++/Beginner Questions/Solution2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int var;
+     var = 5000;
+     cout << "var = "<<var << endl;
+     var = 1000;
+     cout << "var = "<<var << endl;
+}
